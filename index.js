9b2d24b41aa9fdfcc8491cef1c23ef63595cad06
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-06 17:45:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.change_code = 1;
 'use strict';
 
 var alexa = require( 'alexa-app' );
-var app = new alexa.app( 'WelcomeSkill' );
+var app = new alexa.app( 'test-skill' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. ' ).shouldEndSession( false );
+	response.say( 'Welcome, have a good day!!! What would you like to do?' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
 } );
 
 
@@ -17,20 +17,23 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('GetNewWelcomeIntent',
-  {
-    "slots":{"string":"STRING"}
-	,"utterances":[ 
-		"say hi ",
-		"say hello {string}",
-		]
-  },
-  function(request,response) {
-    var number = request.slot('string');
-    response.say("Hello "+string+"Good evening");
+// app.intent('sayNumber',
+//   {
+//     "slots":{"number":"NUMBER"}
+// 	,"utterances":[ 
+// 		"say the number {1-100|number}",
+// 		"give me the number {1-100|number}",
+// 		"tell me the number {1-100|number}",
+// 		"I want to hear you say the number {1-100|number}"]
+//   },
+//   function(request,response) {
+//     var number = request.slot('number');
+//     response.say("You asked for the number "+number);
+//   }
+// );
+app.intent('GetNewWelcomeIntent',function(request,response) {
+    var name = request.slot('name');
+    response.say("Your name is "+zodiac+" Thank You!!! have a good day");
   }
 );
-
 module.exports = app;
-
-console.log("working");
